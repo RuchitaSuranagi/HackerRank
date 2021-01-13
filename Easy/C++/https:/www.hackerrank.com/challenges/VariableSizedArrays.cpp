0a3cd3c1db4f2168138f33df01cfda793734f0e5
Ruchita Suranagi <55045069+RuchitaSuranagi@users.noreply.github.com>
2021-01-13 12:51:38 +0530

    Create VariableSizedArrays.cpp

---
+++ Easy/C++/https:/www.hackerrank.com/challenges/VariableSizedArrays.cpp
@@ -0,0 +1,32 @@
+//https://www.hackerrank.com/challenges/variable-sized-arrays/problem
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {    
+    int n, q,k;
+    cin>>n>>q;
+    
+    int *a[n];
+    
+    for(int i=0; i<n ; i++){
+        cin>>k;
+        
+        a[i] = new int[k]; 
+        
+        for(int j=0; j<k; j++){
+        cin>>a[i][j]; 
+        } 
+    }
+    
+    for(int i=0; i<q ; i++){
+     int x,y;
+     cin>>x>>y;
+     cout<<a[x][y]<<endl;
+    }
+    
+    return 0;
+}
