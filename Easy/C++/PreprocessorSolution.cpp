ab382feac0a52a9b34cb6b47a0eba8d0b2a1032e
Ruchita Suranagi <55045069+RuchitaSuranagi@users.noreply.github.com>
2021-02-15 21:30:33 +0530

    Create PreprocessorSolution.cpp

---
+++ Easy/C++/PreprocessorSolution.cpp
@@ -0,0 +1,37 @@
+/* Enter your macros here */
+//http://www.cplusplus.com/doc/tutorial/preprocessor/
+#define FUNCTION(MINMAXfn, opr) void MINMAXfn(int &a, int &b){ a=(a)opr(b)?(a):(b);}
+#define foreach(v, i) for(int i=0; i<v.size(); i++)
+#define io(v) cin>>v
+#define INF 10000000
+#define toStr(x) #x
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#if !defined toStr || !defined io || !defined FUNCTION || !defined INF
+#error Missing preprocessor definitions
+#endif 
+
+FUNCTION(minimum, <)
+FUNCTION(maximum, >)
+
+int main(){
+    int n; cin >> n;
+    vector<int> v(n);
+    foreach(v, i) {
+        io(v)[i];
+    }
+    int mn = INF;
+    int mx = -INF;
+    foreach(v, i) {
+        minimum(mn, v[i]);
+        maximum(mx, v[i]);
+    }
+    int ans = mx - mn;
+    cout << toStr(Result =) <<' '<< ans;
+    return 0;
+
+}
